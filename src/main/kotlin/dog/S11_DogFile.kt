3ab7a0ee721a0f01575c8ab49f061b0bb4e65b49
DifferBid <best.apps.worlds@gmail.com>
2024-07-22 15:53:59 +0300

    commit

--- src/main/kotlin/dog/S11_DogFile.kt
+++ src/main/kotlin/dog/S11_DogFile.kt
@@ -1,6 +1,7 @@
 
 package org.example.dog
 
+// Создадим новый файл S11_DogFile.kt в пакете dog и объявим в нем функцию верхнего уровня.
 // Функция верхнего уровня, которая возвращает строку
 fun someFun(param: String): String {
     return "Result: $param"
