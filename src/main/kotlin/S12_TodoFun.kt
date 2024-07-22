b0acc9497dce28c37be28185a1fca0c2faaa8ad1
DifferBid <best.apps.worlds@gmail.com>
2024-07-22 17:01:40 +0300

    commit

--- src/main/kotlin/S12_TodoFun.kt
+++ src/main/kotlin/S12_TodoFun.kt
@@ -5,3 +5,27 @@ package org.example
  * реализована.
  */
 public inline fun TODO(): Nothing = throw NotImplementedError()
+
+fun main(){
+    shouldReturnAString()
+    // Вывод: Exception in thread "main" kotlin.NotImplementedError:
+    shouldReturnADog()
+    // Вывод: Exception in thread "main" kotlin.NotImplementedError:
+    shouldReturnADogOne()
+    // Вывод: Exception in thread "main" kotlin.NotImplementedError:
+}
+
+// Функция, которая должна вернуть строку
+fun shouldReturnAString(): String {
+    TODO("implement the string building functionality here to return a string")
+}
+// Функция, которая должна вернуть объект
+fun shouldReturnADog(): S14Dog {
+    TODO("реализуйте функциональность функции, которая должна возвращать объект класса S14Dog")
+}
+
+// Пример кода с недостижимым участком
+fun shouldReturnADogOne(): S14Dog {
+    TODO()
+    println("миссия невозможна") // этот код не будет вызван
+}
