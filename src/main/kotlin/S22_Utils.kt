c30f16222dd65d33279eb1fb68931ca261450f7c
DifferBid <best.apps.worlds@gmail.com>
2024-07-23 15:05:40 +0300

    commit

--- src/main/kotlin/S22_Utils.kt
+++ src/main/kotlin/S22_Utils.kt
@@ -1,37 +1,62 @@
 
 package org.example
 
+/**-----Example 01-----*/
+import org.example.main.yearNowPlus
+
+/**-----Example 02-----*/
 import org.example.main.yearNowActualPlus
+
+/**-----Example 03-----*/
 // Переименование функции-расширения при импорте
 // Вы можете переименовать функцию-расширение при импорте, используя ключевое слово "as" - yearNowActualMinus as minusYear
 import org.example.main.yearNowActualMinus as minusYear
-import org.example.main.yearNowPlus
+
+/**-----Example 04-----*/
+// Импортируем функцию-расширение и даем ей другое имя (алиас)
+import org.example.main.isEvenNumber as isEven
+
+
 import java.util.Date
 
     fun main() {
         val date = Date()
+
+        /**-----Example 01-----*/
         println(date.yearNowPlus())
         // Вывод 125
         // В Kotlin метод getYear() класса Date возвращает значение, которое может быть неожиданным,
         // если вы привыкли к более современным методам работы с датами. Это связано с тем, что метод getYear() возвращает количество лет,
         // прошедших с 1900 года, а не текущий год в привычном понимании. Поэтому, если текущий год 2024, то date.getYear() вернет 124 + 1 = 125.
 
+        /**-----Example 02-----*/
         // Получаем актуальный год + 1
         println(date.yearNowActualPlus())
         // Вывод 2025
 
+        /**-----Example 03-----*/
         println(date.minusYear())
         // Вывод 2023
 
+        /**-----Example 04-----*/
+        // Вызываем функцию-расширение для числа 4 и выводим результат
+        println(4.isEven())
+        // Вывод к true
+
+        /**-----Example 05-----*/
         // Применяем к строке
         val dog = "Шарик"
         val lastCharacter: Char = dog.lastChar()
         println(lastCharacter)
         // Вывод к
-
     }
 
+    /**-----Example 05-----*/
     // Функция для получения последнего символа из строки
     // Расширение для класса String
     fun String.lastChar(): Char = get(length - 1)
+
+
+
+
 
--- src/main/kotlin/main/S22_MyCustomUtils.kt
+++ src/main/kotlin/main/S22_MyCustomUtils.kt
@@ -2,8 +2,24 @@
 package org.example.main
 import java.util.*
 
-fun Date.yearNowPlus(): Int = year + 1
+    /**-----Example 01-----*/
+    fun Date.yearNowPlus(): Int = year + 1
 
-fun Date.yearNowActualPlus(): Int = year + 1900 + 1
+    /**-----Example 02-----*/
+    fun Date.yearNowActualPlus(): Int = year + 1900 + 1
 
-fun Date.yearNowActualMinus(): Int = year + 1900 - 1
+    /**-----Example 03-----*/
+    fun Date.yearNowActualMinus(): Int = year + 1900 - 1
+
+    /**-----Example 04-----*/
+    // Объявление функции-расширения для типа Int
+    fun Int.isEvenNumber(): Boolean {
+        // Использование встроенных функций div (деление) и times (умножение)
+        // Проверяем, является ли число четным путем деления его на 2 и последующего умножения на 2
+        // Сравниваем результат с исходным числом
+        return this.div(2).times(2) == this
+    }
+    // Функции-расширения позволяют добавлять новые функции к существующим типам без изменения их исходного кода.
+    // В данном примере мы добавляем функцию isEvenNumber к типу Int, которая возвращает true, если число четное, и false в противном случае.
+    // Импортируя функцию с алиасом isEven, мы можем вызывать её более кратко.
+    // Вызов 4.isEven() проверяет, является ли число 4 четным, и выводит результат на экран.
